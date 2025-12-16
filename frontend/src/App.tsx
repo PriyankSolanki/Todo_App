fb100d62720bd6664be1d098b529573377dc8c85
Priyank Solanki <psolanki@condorcet93.fr>
2025-12-16 09:44:51 +0100

    fix : await refetch when rename column

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,6 @@ export default function App() {
         <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
-            <Route path="/b" element={<Board />} />
             <Route
                 path="/board"
                 element={
